a231b9fc219c359906b26b733ba8ab5e21f35a22
targethittinggamer-cell <targethittinggamer@gmail.com>
2026-06-29 00:30:02 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+document.getElementById("startBtn").onclick = function() {
+  document.getElementById("intro").style.display = "none";
+  document.getElementById("main").style.display = "block";
+  document.getElementById("song").play();
+};
